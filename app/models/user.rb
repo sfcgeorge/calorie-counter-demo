d0e9ba7fc7fd2dcff3600a6755a32ff1a3c54fe3
Simon George <simon@sfcgeorge.co.uk>
2015-08-31 19:01:52 +0100

    Remove unnecessary validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,8 @@
 class User < ActiveRecord::Base
   has_secure_password
+  has_secure_token
 
   validates :username, presence: true, length: { in: 2..20 }
-  validates :password_digest, presence: true
   validates :password, presence: true, length: { minimum: 8 }, on: :create
-  validates :token, presence: true
-  validates :admin, presence: true
+  validates :target_calories, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe User do
     it { should have_db_column(:password_digest).of_type(:string) }
     it { should have_db_column(:token).of_type(:string) }
     it { should have_db_column(:admin).of_type(:boolean) }
-    it { should have_db_column(:target_callories).of_type(:integer) }
+    it { should have_db_column(:target_calories).of_type(:integer) }
   end
 
   describe "validations" do
@@ -14,10 +14,8 @@ RSpec.describe User do
 
     it { should validate_presence_of(:username) }
     it { should validate_length_of(:username).is_at_least(2).is_at_most(20) }
-    it { should validate_presence_of(:password_digest) }
     it { should validate_presence_of(:password).on(:create) }
     it { should validate_length_of(:password).is_at_least(8).on(:create) }
-    it { should validate_presence_of(:token) }
-    it { should validate_presence_of(:admin) }
+    it { should validate_presence_of(:target_calories) }
   end
 end
