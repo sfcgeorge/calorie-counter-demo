12d0fcec3d08dbb26bd817e1cba317738a1ad91e
Simon George <simon@sfcgeorge.co.uk>
2015-09-01 14:20:42 +0100

    Add meal validations and spec

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,2 +1,8 @@
 class Meal < ActiveRecord::Base
+  validates :calories, presence: true, numericality: {
+    only_integer: true, less_than_or_equal_to: 30_000
+  }
+  validates :text, presence: true, length: { maximum: 200 }
+  validates :date, presence: true
+  validates :time, presence: true
 end
---
+++ spec/models/meal_spec.rb
@@ -0,0 +1,22 @@
+require "rails_helper"
+
+RSpec.describe Meal do
+  describe "db structure" do
+    it { should have_db_column(:calories).of_type(:integer) }
+    it { should have_db_column(:text).of_type(:string) }
+    it { should have_db_column(:date).of_type(:date) }
+    it { should have_db_column(:time).of_type(:time) }
+  end
+
+  describe "validations" do
+    it { should validate_presence_of(:calories) }
+    it {
+      should validate_numericality_of(:calories)
+        .only_integer.is_less_than_or_equal_to(30_000)
+    }
+    it { should validate_presence_of(:text) }
+    it { should validate_length_of(:text).is_at_most(200) }
+    it { should validate_presence_of(:date) }
+    it { should validate_presence_of(:time) }
+  end
+end
