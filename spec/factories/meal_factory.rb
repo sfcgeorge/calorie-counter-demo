fb5f34758aa485d07c8ef49bbe355152ded2ae66
Simon George <simon@sfcgeorge.co.uk>
2015-09-08 00:13:57 +0100

    Improve seed data

--- spec/factories/meal_factory.rb
+++ spec/factories/meal_factory.rb
@@ -1,12 +1,15 @@
 require "forgery"
 
+ingredients = %w(chicken beef pork lamb duck prawn fish cheese bean tofu)
+meals = %w(pasta salad pie tart pizza wrap bake calzone soup)
+
 # This will guess the User class
 FactoryGirl.define do
   factory :meal do
     user { create(:user) }
     calories { Forgery::Basic.number(at_least: 50, at_most: 2000) }
-    text { Forgery::Basic.text }
+    text { "#{ingredients.sample.capitalize} #{meals.sample}" }
     date { Forgery::Date.date }
-    time { Time.now.strftime("%R") }
+    time { "#{rand(0..23)}:#{rand(0..59)}" }
   end
 end
